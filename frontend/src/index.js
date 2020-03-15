2985c022120c9024cbee7ac4f1567439582aa651
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-03-15 18:52:21 -0400

    add gameplay.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 const newGameButton = document.getElementById("newgame")
 const attackButton = document.getElementById("attack")
-const reGroupButton = document.getElementById("regroup")
 const endTurnButton = document.getElementById("endturn")
 const map = document.getElementById("worldmap")
 
@@ -11,8 +10,10 @@ function renderMap(){
    let imgsrc = "/Users/jesse/Desktop/war/frontend/images/world-map.jpg"
    map.src = imgsrc
    let gameplay = getElementById("gameplay")
-    
 }
+
+
+
 
 
 
