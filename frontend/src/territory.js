07546dbedea6f7381d57a1f9a4b19a229a889aa9
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-03-15 20:11:24 -0400

    continents controller

--- frontend/src/territory.js
+++ frontend/src/territory.js
@@ -1,3 +1,13 @@
 class Territory {
-    
+    static all =[]
+
+    constructor(name){
+        this.name = name
+    }
+
+    fetchTerritories(){
+      return fetch("http://127.0.0.1:3000")
+        .then()
+        .then
+    }
 }
